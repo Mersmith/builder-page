a60efd96688d60e00b3b5aaed0a24cde23f7d796
Mersmith <mersmith14@gmail.com>
2023-10-25 18:53:17 -0500

    Master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WebBuilderComponent } from './web-builder/web-builder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebBuilderComponent
   ],
   imports: [
     BrowserModule,
